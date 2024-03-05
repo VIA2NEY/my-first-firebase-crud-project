1ba3027efaed15b9349ca096df16460aa05c0926
VIA2NEY <kouameangeV11@gmail.com>
2024-03-05 14:11:33 +0000

    first settings of firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_first_firebase_project/firebase_options.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
